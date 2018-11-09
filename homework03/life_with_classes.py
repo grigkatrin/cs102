f24f6f63a7aaa6f0c8d766dce5622b18f2e1d944
grigkatrin <grig.katrin@gmail.com>
2018-11-09 22:56:10 +0300

    Закончена работа над функциями

--- homework03/life_with_classes.py
+++ homework03/life_with_classes.py
@@ -27,10 +27,10 @@ class GameOfLife:
         """ Отрисовать сетку """
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color('black'),
-                    (x, 0), (x, self.height))
+                             (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color('black'),
-                    (0, y), (self.width, y))
+                             (0, y), (self.width, y))
 
     def run(self):
         """ Запустить игру """
@@ -40,7 +40,7 @@ class GameOfLife:
         self.screen.fill(pygame.Color('white'))
 
         # Создание списка клеток
-        # PUT YOUR CODE HERE
+        self.clist = CellList(self.cell_height, self.cell_width, randomize=True)
 
         running = True
         while running:
@@ -73,32 +73,100 @@ class Cell:
 
 
 class CellList:
-
-    def __init__(self, nrows, ncols, randomize=False):
+    def __init__(self, nrows, ncols, randomize=False, is_file=False, file_clist=[]):
         self.nrows = nrows
         self.ncols = ncols
         self.randomize = randomize
-        pass
+        self.clist = []
+
+        if randomize:
+            for i in range(self.nrows):
+                self.clist.append([])
+                for j in range(self.ncols):
+                    self.clist[i].append(Cell(i, j, random.randint(0, 1)))
+        else:
+            for i in range(self.nrows):
+                self.clist.append([])
+                for j in range(self.ncols):
+                    self.clist[i].append(Cell(i, j))
+
+        if is_file:
+            self.clist = file_clist
 
     def get_neighbours(self, cell):
         neighbours = []
-        # PUT YOUR CODE HERE
+        row = cell.row
+        col = cell.col
+        rows = [row - 1, row, row + 1]
+        cols = [col - 1, col, col + 1]
+        for row1 in rows:
+            if 0 <= row1 < self.nrows:
+                for col1 in cols:
+                    if col1 == col and row1 == row:
+                        continue
+                    if 0 <= col1 < self.ncols:
+                        if self.clist[row1][col1].is_alive():
+                            neighbours.append(Cell(row1, col1, True))
+                        else:
+                            neighbours.append(Cell(row1, col1, False))
         return neighbours
 
     def update(self):
         new_clist = deepcopy(self)
+        for row in range(self.nrows):
+            for col in range(self.ncols):
+                neighbours = new_clist.get_neighbours(new_clist.clist[row][col])
+                sum = 0
+                for i in neighbours:
+                    if i.is_alive() == 1:
+                        sum += 1
+                if new_clist.clist[row][col].is_alive() == 0 and sum == 3:
+                    self.clist[row][col] = Cell(row, col, True)
+                elif new_clist.clist[row][col].is_alive() == 1 and (sum == 2 or sum == 3):
+                    self.clist[row][col] = Cell(row, col, True)
+                else:
+                    self.clist[row][col] = Cell(row, col, False)
         # PUT YOUR CODE HERE
         return self
 
     def __iter__(self):
-        pass
+        self.index_row = 0
+        self.index_col = 0
+        return self
 
     def __next__(self):
-        pass
+        if self.index_row < self.nrows:
+            cell = self.clist[self.index_row][self.index_col]
+            self.index_col += 1
+            if self.index_col == self.ncols:
+                self.index_col = 0
+                self.index_row += 1
+            return cell
+        else:
+            raise StopIteration
 
     def __str__(self):
         pass
 
     @classmethod
     def from_file(cls, filename):
-        pass
+        grid = [c for c in open(filename).read()]
+        clist = [[]]
+        c_row = 0
+        c_col = 0
+        ncol = 0
+        nrow = 0
+        for i in grid:
+            if i == '\n':
+                clist.append([])
+                c_row += 1
+                ncol = c_col
+                c_col = 0
+                continue
+            if i == '1':
+                clist[c_row].append(Cell(c_row, c_col, True))
+            if i == '0':
+                clist[c_row].append(Cell(c_row, c_col, False))
+            c_col += 1
+        nrow = c_row + 1
+        return CellList(nrow, ncol, file_clist=clist, is_file=True)
